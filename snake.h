9ab43d6029ccc3cdf62970f808a3eec2767bcb77
StdBaiy <52870900+StdBaiy@users.noreply.github.com>
2020-07-06 09:26:00 +0800

    Update snake.h

--- snake.h
+++ snake.h
@@ -12,12 +12,12 @@ class ChessBoard;
 class snake
 {
 private:
-	int length;//��������
-	int headX,headY, tailX,tailY;//ͷ��β�͵�����
-	int direction[4][2] = { {-1,0},{1,0},{0,-1},{0,1} };//���ڵ������������
-	vector<int> headDirect;//�����ߵ�ǰ�ƶ�����,
-	void move(ChessBoard&cb);//�����ƶ��ĺ���
-	int speed,MaxSpeed;//��λ:ms,ÿ����speedʱ��֮����ǰ��һ��
+	int length;
+	int headX,headY, tailX,tailY;
+	int direction[4][2] = { {-1,0},{1,0},{0,-1},{0,1} };
+	vector<int> headDirect;
+	void move(ChessBoard&cb);
+	int speed,MaxSpeed;
 public:
 	snake(ChessBoard&cb);
 	void reload();
