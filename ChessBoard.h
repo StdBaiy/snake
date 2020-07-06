c43fafaf91469d38a7c9e6953d9efefdea584d39
StdBaiy <52870900+StdBaiy@users.noreply.github.com>
2020-07-06 09:23:19 +0800

    Update ChessBoard.h

--- ChessBoard.h
+++ ChessBoard.h
@@ -1,23 +1,25 @@
 #pragma once
-#include"snake.h"
-#define WIDTH 22
-#define HEIGHT 22
+#include <windows.h>
+#include<cstdio>
+#include"ChessBoard.h"
+#include<vector>
+using namespace std;
 
 extern void PrintChar(const char* ch, UINT count, UINT x, UINT y);
 
-class ChessBoard
-{		
-	friend class snake;
-public:
-	void makeFood(int i, int len);//i�����������,len�����峤��
-	ChessBoard();
+class ChessBoard;
+
+class snake
+{
 private:
-	void newGame();
-	char board [WIDTH][HEIGHT];//����,���ڱ�����Ϸ��Ҫ��Ϣ	
-	//���̵ı߽���#��ʾ,�����ߵĵط��ÿո��ʾ,�ߵ�������*��ʾ,ʳ����$��ʾ
-	//�ߵ����в��������ڸı�board������
-	int foodX[3], foodY[3];
-	//EatenFood������������,��Ӧ���ϵ�����ʳ��;
-	int score, MaxScore;
+	int length;
+	int headX,headY, tailX,tailY;
+	int direction[4][2] = { {-1,0},{1,0},{0,-1},{0,1} };
+	vector<int> headDirect;
+	void move(ChessBoard&cb);
+	int speed,MaxSpeed;
+public:
+	snake(ChessBoard&cb);
+	void reload();
 };
 
